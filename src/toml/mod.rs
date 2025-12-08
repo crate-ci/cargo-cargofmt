def9ecfc66d9629864f8a0080690e9943829adcc
Ed Page <eopage@gmail.com>
2025-12-08 15:58:43 -0600

    feat: Normalize space separators

--- src/toml/mod.rs
+++ src/toml/mod.rs
@@ -5,3 +5,37 @@ pub use tokens::ScalarKind;
 pub use tokens::TokenKind;
 pub use tokens::TomlToken;
 pub use tokens::TomlTokens;
+
+pub struct TokenIndices {
+    i: usize,
+}
+
+impl TokenIndices {
+    pub fn new() -> Self {
+        Self { i: 0 }
+    }
+
+    pub fn next_index(&mut self, tokens: &TomlTokens<'_>) -> Option<usize> {
+        if self.i < tokens.len() {
+            let i = self.i;
+            self.i += 1;
+            Some(i)
+        } else {
+            None
+        }
+    }
+
+    pub fn reset(&mut self, i: usize) {
+        self.i = i;
+    }
+
+    pub fn rev(&self) -> impl Iterator<Item = usize> {
+        (0..self.i).rev()
+    }
+}
+
+impl Default for TokenIndices {
+    fn default() -> Self {
+        Self::new()
+    }
+}
