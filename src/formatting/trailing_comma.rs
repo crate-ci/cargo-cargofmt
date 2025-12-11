160cb4d7ca77aeab9cd72448221c8e69713e53b2
Ed Page <eopage@gmail.com>
2025-12-11 14:40:12 -0600

    feat(formatting): Adjust trailing commas
    Part of #8

--- src/formatting/trailing_comma.rs
+++ src/formatting/trailing_comma.rs
@@ -1,9 +1,92 @@
 use crate::config::lists::SeparatorTactic;
+use crate::toml::TokenKind;
+use crate::toml::TomlToken;
 
 /// Assumptions:
 /// - blocks are reflowed
 #[tracing::instrument]
-pub fn adjust_trailing_comma(_tokens: &mut crate::toml::TomlTokens<'_>, _tactic: SeparatorTactic) {}
+pub fn adjust_trailing_comma(tokens: &mut crate::toml::TomlTokens<'_>, tactic: SeparatorTactic) {
+    let mut indices = crate::toml::TokenIndices::new();
+    while let Some(mut i) = indices.next_index(tokens) {
+        match tokens.tokens[i].kind {
+            TokenKind::StdTableOpen | TokenKind::ArrayTableOpen => {}
+            TokenKind::ArrayOpen | TokenKind::InlineTableOpen => {}
+            TokenKind::StdTableClose | TokenKind::ArrayTableClose => {}
+            TokenKind::ArrayClose => {
+                if let Some(prev_i) = indices.rev().skip(1).find(|i| {
+                    matches!(
+                        tokens.tokens[*i].kind,
+                        TokenKind::ValueSep
+                            | TokenKind::Scalar
+                            | TokenKind::ArrayClose
+                            | TokenKind::InlineTableClose
+                    )
+                }) {
+                    let prev_kind = tokens.tokens[prev_i].kind;
+                    let action = match (tactic, prev_kind) {
+                        (SeparatorTactic::Always, TokenKind::ValueSep) => None,
+                        (SeparatorTactic::Always, _) => Some(Action::Add),
+                        (SeparatorTactic::Never, TokenKind::ValueSep) => Some(Action::Remove),
+                        (SeparatorTactic::Never, _) => None,
+                        (SeparatorTactic::Vertical, TokenKind::ValueSep) => {
+                            if tokens.tokens[prev_i..i]
+                                .iter()
+                                .any(|t| t.kind == TokenKind::Newline)
+                            {
+                                None
+                            } else {
+                                Some(Action::Remove)
+                            }
+                        }
+                        (SeparatorTactic::Vertical, _) => {
+                            if tokens.tokens[prev_i..i]
+                                .iter()
+                                .any(|t| t.kind == TokenKind::Newline)
+                            {
+                                Some(Action::Add)
+                            } else {
+                                None
+                            }
+                        }
+                    };
+                    match action {
+                        Some(Action::Add) => {
+                            tokens.tokens.insert(prev_i + 1, TomlToken::VAL_SEP);
+                            i += 1;
+                            indices.reset(i + 1);
+                        }
+                        Some(Action::Remove) => {
+                            tokens.tokens.remove(prev_i);
+                            i -= 1;
+                            indices.reset(i + 1);
+                            if tokens.tokens[prev_i].kind == TokenKind::Whitespace {
+                                tokens.tokens.remove(prev_i);
+                                i -= 1;
+                                indices.reset(i + 1);
+                            }
+                        }
+                        None => {}
+                    }
+                }
+            }
+            TokenKind::InlineTableClose => {}
+            TokenKind::SimpleKey => {}
+            TokenKind::KeySep => {}
+            TokenKind::KeyValSep => {}
+            TokenKind::Scalar => {}
+            TokenKind::ValueSep => {}
+            TokenKind::Whitespace => {}
+            TokenKind::Comment => {}
+            TokenKind::Newline => {}
+            TokenKind::Error => {}
+        }
+    }
+}
+
+enum Action {
+    Add,
+    Remove,
+}
 
 #[cfg(test)]
 mod test {
@@ -88,26 +171,26 @@ multi-vertical-trailing = [
 
 empty = []
 
-single-horizontal = [1]
+single-horizontal = [1,]
 
 single-horizontal-trailing = [1, ]
 
 single-vertical = [
-    1
+    1,
 ]
 
 single-vertical-trailing = [
     1,
 ]
 
-multi-horizontal = [1, 2, 3]
+multi-horizontal = [1, 2, 3,]
 
 multi-horizontal-trailing = [1, 2, 3, ]
 
 multi-vertical = [
     1,
     2,
-    3
+    3,
 ]
 
 multi-vertical-trailing = [
@@ -161,19 +244,19 @@ empty = []
 
 single-horizontal = [1]
 
-single-horizontal-trailing = [1, ]
+single-horizontal-trailing = [1]
 
 single-vertical = [
     1
 ]
 
 single-vertical-trailing = [
-    1,
+    1
 ]
 
 multi-horizontal = [1, 2, 3]
 
-multi-horizontal-trailing = [1, 2, 3, ]
+multi-horizontal-trailing = [1, 2, 3]
 
 multi-vertical = [
     1,
@@ -184,7 +267,7 @@ multi-vertical = [
 multi-vertical-trailing = [
     1,
     2,
-    3,
+    3
 ]
 
 "#]],
@@ -232,10 +315,10 @@ empty = []
 
 single-horizontal = [1]
 
-single-horizontal-trailing = [1, ]
+single-horizontal-trailing = [1]
 
 single-vertical = [
-    1
+    1,
 ]
 
 single-vertical-trailing = [
@@ -244,12 +327,12 @@ single-vertical-trailing = [
 
 multi-horizontal = [1, 2, 3]
 
-multi-horizontal-trailing = [1, 2, 3, ]
+multi-horizontal-trailing = [1, 2, 3]
 
 multi-vertical = [
     1,
     2,
-    3
+    3,
 ]
 
 multi-vertical-trailing = [
